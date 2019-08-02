e76355a7c69e56f4e8ca00233fcb446cb40462df
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2019-08-02 11:48:41 +0200

    chore: update the displayed message
    Signed-off-by: Cosmin Cojocar <cosmin.cojocar@gmx.ch>

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	title := "Jenkins X golang http example built by Tekton pipeline"
 
 	from := ""
 	if r.URL != nil {
